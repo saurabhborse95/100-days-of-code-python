3888af76274232bcc3a0bfa581779117a7664ec7
saurabh_borse95 <55631426+saurabhborse95@users.noreply.github.com>
2026-02-06 13:53:23 +0100

    Enhance practises with string and input examples
    Added examples of string manipulation, input usage, and variable handling.

--- 01_day_variables_in_python/practise.py
+++ 01_day_variables_in_python/practise.py
@@ -1,1 +1,30 @@
+# Printing Console
+print("Hello World")
 
+
+# String Manipulation
+
+print("Hello World\nHello World next line")
+print("Hello" + " " + "Saurabh")
+
+
+# Python Input Function usage
+print("The input is " + input("What is the input ?") + "!")
+
+
+
+# Python Variables
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+age = 31   # This overwrites the variable to fixed value
+
+print(name)
+print(age)
+# print("Length of the name is " + str(len(name)))
+
+print("Length of your name is : " + str(len(input("Enter your name : "))) )
+
+
+username = input("Enter your name: ")
+length = len(username)
+print(length)
