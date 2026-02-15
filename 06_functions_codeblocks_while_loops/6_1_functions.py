6f75940474d3fa01e3201a6a713ead22303c1b20
saurabh_borse95 <saurabhborse95@gmail.com>
2026-02-15 11:04:09 +0100

    Create 6_1_functions.py

---
+++ 06_functions_codeblocks_while_loops/6_1_functions.py
@@ -0,0 +1,24 @@
+### Creating own functions
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+### Triggering a function
+my_function()           # Calling the function
+
+## def my_function():
+#       Do this
+        #then do this
+        #finally do this
+
+
+##### Indentation
+
+### While Loop
+
+
+## while something_istrue:
+        # do something
+
